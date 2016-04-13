d77eab87d9ef1f41ce1246fb4e842bee2629738b
billwear <wowear@gmail.com>
2016-04-13 13:18:46 -0500

    Added files via upload

--- README.md
+++ README.md
@@ -1,10 +1,43 @@
 # erp
-erp = effective rotary phone; 
-**calls up** various pieces of useful information, as from a planner. 
-*note the pun.*
 
-erp version **0.1**, copyright (c) 2016 by billwear; 
-MIT License, no warranty
+erp 0.11 - Copyright (C) 2016, Bill Wear
+   [e]ffective [r]otary [p]hone - consults '.phonebook' for stored info
+   erp is licensed under the MIT License, with no warranty.
+
+   -d designator show items of type <designator> (see below)
+   -t tag        show only items containing <tag>
+   -f phonebook  load <phonebook> instead of ~/.phonebook
+   -v            verbose output
+   -h            print this usage message and exit
+
+   phonebook structure:
+      designator\tinformation
+      \tcontinuation of last designator
+
+   where <designator> is one of the following:
+      name   someone's contact information
+      [date] a date in one of the formats described below
+      note   a note to keep & call up later
+      todo   a to-do item
+
+   date formats:
+      yyyymmdd  a specific date; substituting dashes for a digit has the
+                effect of ignoring that part of the date (allowing repeats);
+                for example, 0000mmdd = annual; 000000dd = ddth day of each month.
+
+      mon       name of any day; only looks at first three letters of the day
+
+      Nmon      Nth day of every month (eg, Nth monday); only looks at first
+                three letters of the day; nonsense numbers match last such day.
+
+      Nmon oct  Nth day of given month; month can be string or number, only matches
+                first three letters of a string; nonsense month = every month.
+
+      weekday   every monday through friday; holidays not considered, so ymmv.
+
+      weekend   every saturday and sunday.
+
+      Nweekend  Nth weekend of every month; defaults to last if N is nonsensical.
 
 **version history**
 <table><tr>
@@ -12,6 +45,7 @@ MIT License, no warranty
 </tr><tr>
 <td>0.1</td><td>20160405</td><td>screens & captures command line options<br>
 (gotta start somewhere, right?)</td>
+<td>0.11</td><td>20160413</td><td>prints usage; reads phonebook file & prints it out<br>
 </tr>
 </table>
 
--- erp.pl
+++ erp.pl
@@ -8,24 +8,44 @@ use Getopt::Long;
 Getopt::Long::Configure qw(gnu_getopt);
 use Encode;
 
-# what global vars would i like to have?
+# what are the global variables?
 our $targetDates = "today"; 
-our $regex = "*";      
-our $altCalendar = ".bigmeal";
+our $matchingTag = "*";      
+our $phonebook   = ".phonebook";
 our $verboseFlag = 0;
-our $helpFlag = 0;
-our $usageMsg = 0;
+our $helpFlag    = 0;
+our $usageMsg    = 0;
 
 # what's the usage message?
 $usageMsg = 
 	  "erp 0.1 - Copyright (C) 2016, Bill Wear\n"
+   . "   [e]ffective [r]otary [p]hone - consults '.phonebook' for stored info\n"
 	. "   erp is licensed under the MIT License, with no warranty.\n\n"
-	. "   -d dates    show events only for the given date range\n"
-   . "               e.g, 20160404, apr 4, apr 4-16, etc.\n"
-   . "   -r regex    show only events containing regex, obeying -r\n"
-   . "   -f meal     load <meal> instead of ~/.bigmeal\n"
-   . "   -v          verbose output\n"
-   . "   -h          print this usage message and exit\n\n";
+	. "   -d designator show items of type <designator> (see below)\n"
+   . "   -t tag        show only items containing <tag>\n"
+   . "   -f phonebook  load <phonebook> instead of ~/.phonebook\n"
+   . "   -v            verbose output\n"
+   . "   -h            print this usage message and exit\n\n"
+	. "phonebook structure:\n"
+   . "   designator\\tinformation\n"
+   . "   \\tcontinuation of last designator\n\n"
+   . "where <designator> is one of the following:\n"
+   . "   name   someone's contact information\n"
+	. "   [date] a date in one of the formats described below\n"
+   . "   note   a note to keep & call up later\n"
+   . "   todo   a to-do item\n\n"
+	. "date formats:\n"
+   . "   yyyymmdd  a specific date; substituting dashes for a digit has the \n"
+   . "             effect of ignoring that part of the date (allowing repeats);\n"
+   . "             for example, 0000mmdd = annual; 000000dd = ddth day of each month.\n\n"
+   . "   mon       name of any day; only looks at first three letters of the day.\n\n"
+   . "   Nmon      Nth day of every month (eg, Nth monday); only looks at first \n"
+   . "             three letters of the day; nonsense numbers match last such day.\n\n"
+   . "   Nmon oct  Nth day of given month; month can be string or number, only matches\n"
+   . "             first three letters of a string; nonsense month = every month.\n\n"
+   . "   weekday   every monday through friday; holidays not considered, so ymmv.\n\n"
+   . "   weekend   every saturday and sunday.\n\n"
+   . "   Nweekend  Nth weekend of every month; defaults to last if N is nonsensical.\n\n";
   
 
 # subs - listed above to squelch prototype complaints
@@ -33,11 +53,19 @@ sub doCmdLineArgs()
 {
 	GetOptions(
 		'd=s' => \$targetDates, # one date or a range of dates
-		'r=s' => \$regex,       # filter lines by a string
-		'f=s' => \$altCalendar, # instead of ~/.bigmeal
+		't=s' => \$matchingTag, # filter lines by a string
+		'f=s' => \$phonebook,   # instead of ~/.phonebook
 		'v'   => \$verboseFlag, # verbose mode
 		'h'   => \$helpFlag     # print usage and quit
 	) or die $usageMsg;
+
+	if($verboseFlag) {
+		print "target dates = $targetDates\n";
+		print "regex        = $matchingTag\n";
+		print "phonebook    = $phonebook\n";
+		print "verbose      = $verboseFlag\n";
+		print "help         = $helpFlag\n";
+	}
 }
 
 ### main thread
@@ -45,8 +73,21 @@ sub doCmdLineArgs()
 # are there command line args?
 doCmdLineArgs( );
 
-print "target dates = $targetDates\n";
-print "regex        = $regex\n";
-print "meal file    = $altCalendar\n";
-print "verbose      = $verboseFlag\n";
-print "help         = $helpFlag\n";
+if( $helpFlag ) {
+	print $usageMsg;
+	exit;
+}
+
+# found the phone book?
+open my $fh, "<", $phonebook
+	or die "can't open phonebook $phonebook: $!";
+
+# for every line in the phonebook...
+while( my $line = <$fh> ) {
+	print $line;
+
+}
+
+# close the phone book
+close $fh; 
+
